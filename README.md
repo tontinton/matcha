aaac3327ed9e2e42cf34d53ad39e265698ce2ac1
Tony Solomonik <tony.solomonik@gmail.com>
2022-12-03 22:17:00 +0200

    Serve http POST that runs the javascript code in body

---
+++ README.md
@@ -0,0 +1,11 @@
+# Matcha
+
+A minimal serverless javascript runtime.
+
+## To run the example
+Run `matcha`
+
+Send a POST to the server with the body containing the javascript code you want to run:
+```bash
+curl -d @example.js localhost:8080/
+```
--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,28 @@
-use deno_core::error::AnyError;
+use actix_web::{web, App, HttpServer};
+use deno_core::anyhow::Error;
 use deno_core::op;
 use deno_core::Extension;
 use std::rc::Rc;
 
 #[op]
-async fn op_read_file(path: String) -> Result<String, AnyError> {
+async fn op_read_file(path: String) -> Result<String, Error> {
     let contents = tokio::fs::read_to_string(path).await?;
     Ok(contents)
 }
 
 #[op]
-async fn op_write_file(path: String, contents: String) -> Result<(), AnyError> {
+async fn op_write_file(path: String, contents: String) -> Result<(), Error> {
     tokio::fs::write(path, contents).await?;
     Ok(())
 }
 
 #[op]
-fn op_remove_file(path: String) -> Result<(), AnyError> {
+fn op_remove_file(path: String) -> Result<(), Error> {
     std::fs::remove_file(path)?;
     Ok(())
 }
 
-async fn run_js(file_path: &str) -> Result<(), AnyError> {
-    let main_module = deno_core::resolve_path(file_path)?;
+async fn run_js(code: &str) -> Result<(), Error> {
     let runjs_extension = Extension::builder()
         .ops(vec![
             op_read_file::decl(),
@@ -35,22 +35,33 @@ async fn run_js(file_path: &str) -> Result<(), AnyError> {
         extensions: vec![runjs_extension],
         ..Default::default()
     });
+
+    // Init runtime.
     runtime
-        .execute_script("[runjs:runtime.js]", include_str!("./runtime.js"))
+        .execute_script("[matcha:runtime.js]", include_str!("./runtime.js"))
         .unwrap();
 
-    let mod_id = runtime.load_main_module(&main_module, None).await?;
+    // Load the javascript code as a module to allow async / await
+    let spec = deno_core::resolve_url("file:///main.js").unwrap();
+    let mod_id = runtime
+        .load_main_module(&spec, Some(code.to_owned()))
+        .await?;
     let result = runtime.mod_evaluate(mod_id);
     runtime.run_event_loop(false).await?;
     result.await?
 }
 
-fn main() {
-    let runtime = tokio::runtime::Builder::new_current_thread()
-        .enable_all()
-        .build()
-        .unwrap();
-    if let Err(error) = runtime.block_on(run_js("./example.js")) {
-        eprintln!("error: {}", error);
+async fn run_js_route(req_body: String) -> actix_web::Result<&'static str> {
+    if let Err(e) = run_js(req_body.as_str()).await {
+        return Err(actix_web::error::ErrorBadRequest(e.to_string()));
     }
+    Ok("Enjoy a cup of matcha!")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().route("/", web::post().to(run_js_route)))
+        .bind(("127.0.0.1", 8080))?
+        .run()
+        .await
 }
